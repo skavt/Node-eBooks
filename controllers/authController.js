5f1c93a6e505ea6d982d5597a43383b4482b31f7
Salome Kavtaradze <s_kavtaradze2@cu.edu.ge>
2022-04-06 19:41:13 +0400

    Delete extra try catch

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,24 +1,19 @@
-const logger = require('../logger');
 const userService = require('../service/authService')
 
 const login = async (req, res) => {
-  if (Object.keys(req.body).length < 2) {
+  const userData = req.body;
+
+  if (Object.keys(userData).length < 2) {
     res.send(JSON.stringify({message: 'Email and Password are required'}));
     return;
   }
-  const userData = req.body;
-  try {
-    const {success, user, message} = await userService.login(userData);
 
-    if (success) {
-      res.send(JSON.stringify(user));
-    } else {
-      res.send(JSON.stringify({message}));
-    }
-  } catch (e) {
-    logger.error(e);
-    res.status(500);
-    res.send(JSON.stringify({message: e}));
+  const {success, user, message} = await userService.login(userData);
+
+  if (success) {
+    res.send(JSON.stringify(user));
+  } else {
+    res.send(JSON.stringify({message}));
   }
 }
 
