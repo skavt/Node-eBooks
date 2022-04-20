ce48e02a1f4b8bc87de93227becd5f06a76c8125
Salome Kavtaradze <s_kavtaradze2@cu.edu.ge>
2022-04-20 11:41:14 +0400

    Add nodemailer to send notifications to mail

---
+++ notifications/index.js
@@ -0,0 +1,25 @@
+const nodemailer = require("nodemailer");
+
+let transporter = nodemailer.createTransport({
+  host: process.env.EMAIL_HOST,
+  port: process.env.EMAIL_PORT,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_HOST_USER,
+    pass: process.env.EMAIL_HOST_PASSWORD,
+  },
+});
+
+const sendMail = async (to, subject, textMsg, htmlMsg) => {
+  return await transporter.sendMail({
+    from: process.env.EMAIL_ROBOT_EMAIL,
+    to: to,
+    subject: subject,
+    text: textMsg,
+    html: htmlMsg,
+  });
+}
+
+module.exports = {
+  sendMail,
+}
